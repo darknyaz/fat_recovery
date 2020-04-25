d6e9623a29ec324da4a46c1d177e1ada3681aa6a
Vladimir Knyazev <darknyaz@yandex.ru>
2020-04-25 13:22:59 +0300

    Add more fields in fat_boot_sector

--- fat_recover.py
+++ fat_recover.py
@@ -5,16 +5,6 @@
 import struct
 
 
-BYTES_IN_SECTOR_OFFSET = 11
-BYTES_IN_SECTOR_SIZE = 2
-
-SECTORS_IN_CLUSTER_OFFSET = 13
-SECTORS_IN_CLUSTER_SIZE = 1
-
-RESERVED_REGION_SIZE_IN_SECTORS_OFFSET = 14
-RESERVED_REGION_SIZE_IN_SECTORS_SIZE = 2
-
-
 FAT_BOOT_SECTOR = {
     'bytes_in_sector': {
         'offset': 11,
@@ -30,13 +20,24 @@ FAT_BOOT_SECTOR = {
         'offset': 14,
         'size': 2,
         'value': 0
+    },
+    'number_of_fat_copies': {
+        'offset': 16,
+        'size': 1,
+        'value': 0
+    },
+    'fat_size_in_sectors': {
+        'offset': 36,
+        'size': 4,
+        'value': 0
     }
 }
 
 
 STRUCT_UNPACK_FORMAT_LETTERS = {
     1: 'B',
-    2: 'H'
+    2: 'H',
+    4: 'I'
 }
 
 
@@ -60,6 +61,15 @@ def read_boot_sector(fat_dump_file):
         fat_dump_file, FAT_BOOT_SECTOR['reserved_region_size_in_sectors']
     )
 
+    # read number_of_fat_copies
+    read_field_from_dump(
+        fat_dump_file, FAT_BOOT_SECTOR['number_of_fat_copies']
+    )
+
+    # read fat_size_in_sectors
+    read_field_from_dump(
+        fat_dump_file, FAT_BOOT_SECTOR['fat_size_in_sectors']
+    )
 
 if __name__ == "__main__":
     fat_dump_file = open('fat32.dd', 'rb')
